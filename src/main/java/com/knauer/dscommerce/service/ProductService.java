2748169b0d3aef3ab136016c6c34794f6ecefd39
unknown <marcelocrisknauer@gmail.com>
2023-02-26 22:35:06 -0300

    Delete Product

--- src/main/java/com/knauer/dscommerce/service/ProductService.java
+++ src/main/java/com/knauer/dscommerce/service/ProductService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.knauer.dscommerce.dto.ProductDTO;
@@ -51,5 +52,10 @@ public class ProductService {
 		entity.setDescription(dto.getDescription());
 		entity.setImgUrl(dto.getImgUrl());
 		entity.setPrice(dto.getPrice());
+	}
+
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		repository.deleteById(id);
 	}
 }
