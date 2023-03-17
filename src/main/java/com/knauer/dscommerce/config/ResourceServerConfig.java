4d0741b7285bef164e6b909ce3b4f6f14f24f1c2
unknown <marcelocrisknauer@gmail.com>
2023-03-17 14:37:25 -0300

    Access control

--- src/main/java/com/knauer/dscommerce/config/ResourceServerConfig.java
+++ src/main/java/com/knauer/dscommerce/config/ResourceServerConfig.java
@@ -5,26 +5,33 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
 	private Environment env;
 
+	//private final static String [] PUBLIC = {"/ouath/token", "/h2-console/**"};
+	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 
+		
+		
 		// H2 frames
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
 		
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests()
+		.anyRequest().permitAll();
 		
 	}
 	
